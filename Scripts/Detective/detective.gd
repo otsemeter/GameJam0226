94c891057482859a3cd930c8df506e11c726df7c
Owen Semeter <owensemeter@gmail.com>
2026-02-19 17:30:20 -0500

    some teleportation and walking done

--- Scripts/Detective/detective.gd
+++ Scripts/Detective/detective.gd
@@ -1,9 +1,34 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
+var in_doorway = false
+var tp_x = 0
+var tp_y = 0
 
+func _process(delta) -> void:
+	# handle animations
+	if Input.is_action_pressed("ui_down"):
+		$AnimatedSprite2D.play("FrontWalk")
+	elif Input.is_action_just_released("ui_down"):
+		$AnimatedSprite2D.play("FrontIdle")
+	elif Input.is_action_pressed("ui_up"):
+		$AnimatedSprite2D.play("BackWalk")
+	elif Input.is_action_just_released("ui_up"):
+		$AnimatedSprite2D.play("BackIdle")
+	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite2D.play("SideWalk")
+		$AnimatedSprite2D.flip_h = true
+	elif Input.is_action_just_released("ui_left") or Input.is_action_just_released("ui_right"):
+		$AnimatedSprite2D.play("SideIdle")
+	elif Input.is_action_pressed("ui_right"):
+		$AnimatedSprite2D.play("SideWalk")
+		$AnimatedSprite2D.flip_h = false
+		
+	# handle player teleportation
+	
+		
 
 func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
@@ -20,3 +45,30 @@ func _physics_process(delta: float) -> void:
 		velocity.y = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_kitchen_door_body_entered(body: Node2D) -> void:
+	in_doorway = true
+	tp_y -= 180
+	tp_x += 90
+
+
+func _on_cellar_door_body_entered(body: Node2D) -> void:
+	in_doorway = true
+	tp_y += 200 # Replace with function body.
+
+
+func _on_kitchen_to_hallway_body_entered(body: Node2D) -> void:
+	in_doorway = true
+	tp_y += 180
+	tp_x -= 90
+
+
+func _on_living_room_door_body_entered(body: Node2D) -> void:
+	in_doorway = true
+	tp_x += 150
+
+
+func _on_living_room_to_hallway_body_entered(body: Node2D) -> void:
+	in_doorway = true
+	tp_x -= 150 # Replace with function body.
